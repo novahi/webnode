89147c059aeed868b4a78e0aecdbc6ab086dbf59
novahi <nguoidungemail1@gmail.com>
2021-12-17 15:30:47 +0700

    update 1

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const authenticationController = require('../app/controllers/authenticationController');
+const authenticationController = require('../app/controllers/AuthenticationController');
 
 router.post('/login', authenticationController.login);
 router.post('/sigin', authenticationController.sigin);
