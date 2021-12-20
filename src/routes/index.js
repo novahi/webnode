fc709da02f0f66746c47e0b2caf432cf391cb10d
novahi <53039974+novahi@users.noreply.github.com>
2021-12-21 06:06:16 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,11 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
 const userRouter = require('./user');
-const loginRouter = require('./login');
-const siginRouter = require('./sigin');
+
 
 
 function route(app) {
-    app.use('/login', loginRouter);
-    app.use('/sigin', siginRouter)
+    
     app.use('/news', newsRouter);
     app.use('/users', userRouter);
     app.use('/', siteRouter);
