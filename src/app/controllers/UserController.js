2e81ee05a3b5a537bfbedb4ea95f487f8ed88c8c
novahi <nguoidungemail1@gmail.com>
2021-12-23 17:22:06 +0700

    update env

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,44 +1,75 @@
 const User = require('../models/User')
 class UserController {
-    show(req, res, next) {
-        User.findOne({ slug: req.params.slug })
-            .lean()
-            .then((user) => res.render('viewuser', { user }))
-            .catch(next);
+  async show(req, res, next) {
+    try {
+      const data = await User.findOne({
+        slug: req.params.slug
+      }).lean()
+
+      return res.render('viewuser', {
+        data
+      })
+
+    } catch (e) {
+      next()
     }
-    create(req, res, next) {
-        res.render('create');
+  }
+  async create(req, res, next) {
+    return await res.render('create')
+  }
+  async view(req, res, next) {
+    try {
+      const data = await User.find({}).lean()
+      return res.render('user', {
+        data
+      })
+    } catch (e) {
+      next()
     }
-    view(req, res, next) {
-        User.find({})
-            .lean()
-            .then((data) => res.render('user', { data }))
-            .catch(next);
+  }
+  async store(req, res, next) {
+    try {
+      const formData = await req.body
+      formData.image = `https://graph.facebook.com/${req.body.facebook.trim()}/picture?height=1000&width=1000&ftype=large&${process.env.TOKEN_FB}`
+      const newUser = await new User(formData)
+      const data = await newUser.save()
+      return res.redirect('/users')
+
+
+    } catch (e) {
+      next()
     }
-    store(req, res, next) {
-        const formData = req.body;
-        formData.image = `https://graph.facebook.com/${req.body.facebook.trim()}/picture?height=1000&width=1000&ftype=large&${process.env.TOKEN_FB}`
-        const newUser = new User(formData);
-        newUser
-            .save()
-            .then(() => res.redirect('/users'))
-            .catch((err) => res.send(err));
+  }
+  async edit(req, res, next) {
+    try {
+      const data = await User.findById(req.params.id).lean()
+      return res.render('edit', {
+        data
+      })
+    } catch (e) {
+      next()
     }
-    edit (req, res, next) {
-        User.findById(req.params.id)
-            .lean()
-            .then(data => res.render('edit', {data}))
-            .catch(next)
+  }
+  async update(req, res, next) {
+    try {
+      const data = await User.updateOne({
+        _id: req.params.id
+      }, req.body)
+      return res.redirect('/users')
+    } catch (e) {
+      next()
     }
-    update(req, res, next) {
-        User.updateOne({ _id: req.params.id }, req.body)
-            .then(data => res.redirect('/users'))
+  }
+  async destroy(req, res, next) {
+    try {
+      const data = await User.deleteOne({
+        _id: req.params.id
+      })
+      return res.redirect('back')
+    } catch (e) {
+      next()
     }
-    destroy (req, res, next) {
-        User.deleteOne({ _id: req.params.id })
-            .then(() => res.redirect('back'))
-            .catch(next)
-}
+  }
 }
 
-module.exports = new UserController();
+module.exports = new UserController()
